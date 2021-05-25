f048d9fdc8df51a8f52f8dc40d7bd7a1dd2bbe9e
liwang545@gmail.com <liwang545@gmail.com>
2021-05-25 22:20:21 +0200

    clean code

--- find_missing.py
+++ find_missing.py
@@ -3,8 +3,8 @@
 from typing import List
 
 def find_missing(input_list:List, range_max=20):
-    """This function will return a string 
-    format of missing numbers from a range 
+    """This function will return the missing numbers(in string format) 
+    by comparing the input_list with the range list 
   """
     missing = sorted([x for x in range(range_max+1) if x not in input_list])
 
@@ -12,7 +12,7 @@ def find_missing(input_list:List, range_max=20):
     # get the indices where the difference between adjacent numbers is great than 1
     indices = [i + 1 for i, x in enumerate(subtract) if x !=1]
 
-    # split the list, each sublist has numbers increasing by 1
+    # split the list, each sublist missing[i:j]  has numbers increasing by 1
 
     result_str=[]
     for i, j in zip([0] + indices, indices + [None]):
@@ -22,14 +22,6 @@ def find_missing(input_list:List, range_max=20):
             result_str.append( str(min(missing[i:j])))
 
     result_str = (','.join(result_str))
-    
-    # split_missing = [missing[i:j] for i, j in zip([0] + indices, indices + [None])]
-    # result_str=""
-    # for split in split_missing:
-    #     if len(split)>1:
-    #         result_str += ","+str(min(split))+"-"+str(max(split))
-    #     else:
-    #         result_str+=str(min(split))+","
 
     return result_str
 
